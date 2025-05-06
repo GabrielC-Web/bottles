70b62204cb23df859e92c67afa59e71d99419a8c
GabrielC-Web <dobbyeslibre23@gmail.com>
2025-05-05 23:37:00 -0400

    aver si ya cargan los assets

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@
  * @type {import('vite').UserConfig}
  */
 export default {
-  base: process.env.NODE_ENV === "production" ? "/bottles/assets/" : "",
+  base: process.env.NODE_ENV === "production" ? "/bottles/" : "",
 };
